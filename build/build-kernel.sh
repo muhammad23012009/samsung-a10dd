1e07eef2f638b0e206d897b4e44597c85bfb45bb
Muhammad <77672824+muhammad23012009@users.noreply.github.com>
2022-01-07 02:43:49 +0500

    build: Fix build errors

--- build/build-kernel.sh
+++ build/build-kernel.sh
@@ -29,9 +29,9 @@ MAKEOPTS="CC=clang CLANG_TRIPLE=aarch64-linux-gnu-"
 # fi
 
 cd "$KERNEL_DIR"
-LD_LIBRARY_PATH="$TMPDOWN/proton-clang/lib:$LD_LIBRARY_PATH make O="$OUT" $deviceinfo_kernel_defconfig
-LD_LIBRARY_PATH="$TMPDOWN/proton-clang/lib:$LD_LIBRARY_PATH make O="$OUT" -j$(nproc --all) $MAKEOPTS
-LD_LIBRARY_PATH="$TMPDOWN/proton-clang/lib:$LD_LIBRARY_PATH make O="$OUT" $MAKEOPTS INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH="$INSTALL_MOD_PATH" modules_install
+LD_LIBRARY_PATH="$TMPDOWN/proton-clang/lib:$LD_LIBRARY_PATH" make O="$OUT" $deviceinfo_kernel_defconfig
+LD_LIBRARY_PATH="$TMPDOWN/proton-clang/lib:$LD_LIBRARY_PATH" make O="$OUT" -j$(nproc --all) $MAKEOPTS
+LD_LIBRARY_PATH="$TMPDOWN/proton-clang/lib:$LD_LIBRARY_PATH" make O="$OUT" $MAKEOPTS INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH="$INSTALL_MOD_PATH" modules_install
 ls "$OUT/arch/$ARCH/boot/"*Image*
 
 if [ -n "$deviceinfo_kernel_apply_overlay" ] && $deviceinfo_kernel_apply_overlay; then
